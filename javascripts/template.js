a6a1e26a5b2cf6a618adc6cbf7d8a64908d3d532
Agnieszka Cieśla <acs@touk.pl>
2016-05-04 23:02:25 +0200

    Updates

--- javascripts/template.js
+++ javascripts/template.js
@@ -3,7 +3,7 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 
-buf.push("<!DOCTYPE html><html lang=\"en\"><head><title>droidcon Kraków 2016</title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=0.75, maximum-scale=0.75, user-scalable=0\"><meta name=\"twitter:card\" content=\"summary_large_image\"><meta name=\"twitter:site\" content=\"\"><meta name=\"twitter:title\" content=\"\"><meta name=\"twitter:description\" content=\"\"><meta name=\"twitter:image\" content=\"img/twitter-card.png\"><meta property=\"og:type\" content=\"\"><meta property=\"og:title\" content=\"\"><meta property=\"og:image\" content=\"img/facebook-card.png\"><link rel=\"stylesheet\" href=\"stylesheets/app.css\" type=\"text/css\"><link rel=\"shortcut icon\" href=\"img/icon.png\"><script type=\"text/javascript\" src=\"javascripts/vendor.js\"></script><script type=\"text/javascript\" src=\"javascripts/app.js\"></script><script type=\"text/javascript\">if (window.brunch == null) {\n  window.brunch = {};\n}\nwindow.brunch['auto-reload'] = {\n  enabled: true\n};</script></head><body><div id=\"background-image\"><img src=\"/img/main-background.jpg\"></div><div id=\"main-content\"><div class=\"container\"><div class=\"row\"><div class=\"col-xs-12\"><h1 class=\"title\">droidcon Kraków 2016</h1><h4>coming soon</h4></div></div><div class=\"row contact-us\"><div class=\"col-xs-12\"><a href=\"mailto:contact@droidcon.pl\">Contact us</a></div></div></div></div></body></html>");;return buf.join("");
+buf.push("<!DOCTYPE html><html lang=\"en\"><head><title>droidcon Kraków 2016</title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=0.75, user-scalable=0\"><meta name=\"twitter:card\" content=\"summary_large_image\"><meta name=\"twitter:site\" content=\"\"><meta name=\"twitter:title\" content=\"\"><meta name=\"twitter:description\" content=\"\"><meta name=\"twitter:image\" content=\"img/twitter-card.png\"><meta property=\"og:type\" content=\"\"><meta property=\"og:title\" content=\"\"><meta property=\"og:image\" content=\"img/facebook-card.png\"><link rel=\"stylesheet\" href=\"stylesheets/app.css\" type=\"text/css\"><link rel=\"shortcut icon\" href=\"img/icon.png\"><script type=\"text/javascript\" src=\"javascripts/vendor.js\"></script><script type=\"text/javascript\" src=\"javascripts/app.js\"></script><script type=\"text/javascript\">if (window.brunch == null) {\n  window.brunch = {};\n}\nwindow.brunch['auto-reload'] = {\n  enabled: true\n};</script></head><body><div id=\"background-image\"><img src=\"/img/logo.png\" alt=\"droidcon Kraków main logo\"><a href=\"mailto:contact@droidcon.pl\">Contact us</a></div></body></html>");;return buf.join("");
 };
 if (typeof define === 'function' && define.amd) {
   define([], function() {
@@ -19,7 +19,7 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 
-buf.push("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"/><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/><meta charset=\"utf-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=0.75, maximum-scale=0.75, user-scalable=0\"/><meta name=\"twitter:card\" content=\"summary_large_image\"/><meta name=\"twitter:site\" content=\"\"/><meta name=\"twitter:title\" content=\"\"/><meta name=\"twitter:description\" content=\"\"/><meta name=\"twitter:image\" content=\"img/twitter-card.png\"/><meta property=\"og:type\" content=\"\"/><meta property=\"og:title\" content=\"\"/><meta property=\"og:image\" content=\"img/facebook-card.png\"/><link rel=\"stylesheet\" href=\"stylesheets/app.css\" type=\"text/css\"/><link rel=\"shortcut icon\" href=\"img/icon.png\"/><script type=\"text/javascript\" src=\"javascripts/vendor.js\"></script><script type=\"text/javascript\" src=\"javascripts/app.js\"></script><script type=\"text/javascript\">if (window.brunch == null) {\n  window.brunch = {};\n}\nwindow.brunch['auto-reload'] = {\n  enabled: true\n};</script>");;return buf.join("");
+buf.push("<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"/><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/><meta charset=\"utf-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=0.75, user-scalable=0\"/><meta name=\"twitter:card\" content=\"summary_large_image\"/><meta name=\"twitter:site\" content=\"\"/><meta name=\"twitter:title\" content=\"\"/><meta name=\"twitter:description\" content=\"\"/><meta name=\"twitter:image\" content=\"img/twitter-card.png\"/><meta property=\"og:type\" content=\"\"/><meta property=\"og:title\" content=\"\"/><meta property=\"og:image\" content=\"img/facebook-card.png\"/><link rel=\"stylesheet\" href=\"stylesheets/app.css\" type=\"text/css\"/><link rel=\"shortcut icon\" href=\"img/icon.png\"/><script type=\"text/javascript\" src=\"javascripts/vendor.js\"></script><script type=\"text/javascript\" src=\"javascripts/app.js\"></script><script type=\"text/javascript\">if (window.brunch == null) {\n  window.brunch = {};\n}\nwindow.brunch['auto-reload'] = {\n  enabled: true\n};</script>");;return buf.join("");
 };
 if (typeof define === 'function' && define.amd) {
   define([], function() {
@@ -35,7 +35,7 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 
-buf.push("<div id=\"background-image\"><img src=\"/img/main-background.jpg\"/></div><div id=\"main-content\"><div class=\"container\"><div class=\"row\"><div class=\"col-xs-12\"><h1 class=\"title\">droidcon Kraków 2016</h1><h4>coming soon</h4></div></div><div class=\"row contact-us\"><div class=\"col-xs-12\"><a href=\"mailto:contact@droidcon.pl\">Contact us</a></div></div></div></div>");;return buf.join("");
+buf.push("<div id=\"background-image\"><img src=\"/img/logo.png\" alt=\"droidcon Kraków main logo\"/><a href=\"mailto:contact@droidcon.pl\">Contact us</a></div>");;return buf.join("");
 };
 if (typeof define === 'function' && define.amd) {
   define([], function() {
@@ -51,7 +51,7 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 
-buf.push("<div id=\"background-image\"><img src=\"/img/main-background.jpg\"/></div><div id=\"main-content\"><div class=\"container\"><div class=\"row\"><div class=\"col-xs-12\"><h1 class=\"title\">droidcon Kraków 2016</h1><h4>coming soon</h4></div></div><div class=\"row contact-us\"><div class=\"col-xs-12\"><a href=\"mailto:contact@droidcon.pl\">Contact us</a></div></div></div></div>");;return buf.join("");
+buf.push("<div id=\"background-image\"><img src=\"/img/logo.png\" alt=\"droidcon Kraków main logo\"/><a href=\"mailto:contact@droidcon.pl\">Contact us</a></div>");;return buf.join("");
 };
 if (typeof define === 'function' && define.amd) {
   define([], function() {
