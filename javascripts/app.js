cf41a7ab2c145981b1e93f1f09ac85931cb5e5b4
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2016-05-17 13:05:59 +0200

    Updates

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-  console.log('test')
+  // console.log('test')
 });
-
-var ghpages = require('gh-pages');
-var path = require('path');
 
 
 //# sourceMappingURL=app.js.map
